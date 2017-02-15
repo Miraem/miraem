da25b09b16997e279d4f984d937b75ca02fb6d0c
Miraem <miraem7@gmail.com>
2017-02-15 17:24:19 +0300

    Counting number of twos

---
+++ src/main/java/com/miraem/CountingTwos.java
@@ -0,0 +1,35 @@
+package com.miraem;
+
+public class CountingTwos {
+    public static int count2sInRangeAtDigit(int number, int d) {
+        int powerOf10 = (int) Math.pow(10, d);
+        int nextPowerOf10 = powerOf10 * 10;
+        int right = number % powerOf10;
+
+        int roundDown = number - number % nextPowerOf10;
+        int roundUp = roundDown + nextPowerOf10;
+
+        int digit = (number / powerOf10) % 10;
+        if (digit < 2) { // если digit меньше 2
+            return roundDown / 10;
+        } else if (digit == 2) {
+            return roundDown / 10 + right + 1;
+        } else {
+            return roundUp / 10;
+        }
+    }
+
+    public static int count2sInRange(int number) {
+        int count = 0;
+        int len = String.valueOf(number).length();
+        for (int digit = 0; digit < len; digit++) {
+            count += count2sInRangeAtDigit(number, digit);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count2sInRange(1002));
+    }
+
+}
