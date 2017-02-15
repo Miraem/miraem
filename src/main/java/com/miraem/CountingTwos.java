f4667c53d31c04203964aaa96c6d54e255662969
Miraem <miraem7@gmail.com>
2017-02-15 21:31:29 +0300

    Updating Count

--- src/main/java/com/miraem/CountingTwos.java
+++ src/main/java/com/miraem/CountingTwos.java
@@ -1,42 +1,15 @@
 package com.miraem;
 
 public class CountingTwos {
-    private static int count2sInRangeAtDigit(int number, int d) {
-        int powerOf10 = (int) Math.pow(10, d);
-        int nextPowerOf10 = powerOf10 * 10;
-        int right = number % powerOf10;
-
-        int roundDown = number - number % nextPowerOf10;
-        int roundUp = roundDown + nextPowerOf10;
-
-        int digit = (number / powerOf10) % 10;
-        if (digit < 2) { // если digit меньше 2
-            return roundDown / 10;
-        } else if (digit == 2) {
-            return roundDown / 10 + right + 1;
-        } else {
-            return roundUp / 10;
-        }
-    }
-
-    private static int count2sInRange(int number) {
+    private static int countOf2(int n) {
         int count = 0;
-        int len = String.valueOf(number).length();
-        for (int digit = 0; digit < len; digit++) {
-            count += count2sInRangeAtDigit(number, digit);
-        }
-        return count;
-    }
-    //  В лоб.
-    private static int numberOf2sInRange(int n) {
-        int count = 0;
-        for (int i = 2; i <= n; i++) { // Можем начать с 2
-            count += numberOf2s(i);
+        for (int i = 2; i <= n; i++) {
+            count += counting(i);
         }
         return count;
     }
     /* подсчитываем число '2' в одном числе */
-    private static int numberOf2s(int n) {
+    private static int counting(int n) {
         int count = 0;
         while (n > 0) {
             if (n % 10 == 2) {
@@ -49,12 +22,8 @@ public class CountingTwos {
 
     public static void main(String[] args) {
         int n = 1000;
-        //В лоб
-        System.out.println(numberOf2sInRange(n));
-        //Через жопу.
-        System.out.println(count2sInRange(n));
 
-
-    }
+        System.out.println(countOf2(n));
+   }
 
 }
